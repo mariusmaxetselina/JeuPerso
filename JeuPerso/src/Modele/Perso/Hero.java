1582726685047179cf64702354f53cf5e936377e
Marius <maxetselina@gmail.com>
2016-05-02 17:45:10 +0200

    j'ai push pleins de choses

--- JeuPerso/src/Modele/Perso/Hero.java
+++ JeuPerso/src/Modele/Perso/Hero.java
@@ -9,6 +9,20 @@ public class Hero extends Personnage {
 	}
 
 	public void fuir(){
-		
+
+	}
+
+	@Override
+	public void comportementCombat(Personnage personnage) {
+		// TODO Auto-generated method stub
+		int i = (int)(Math.random()*3);
+		switch(i){
+		case 1:
+			System.out.println("hero :"+attaquerCritique(personnage));
+			break;
+		default:
+			System.out.println("hero :"+attaquer(personnage));
+			break;
+		}
 	}
 }
