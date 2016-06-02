8250f3b9650d9067ac551389efada131aa52efe7
Marius <maxetselina@gmail.com>
2016-06-02 16:15:45 +0200

    test

--- JeuPerso/src/Vue/Main.java
+++ JeuPerso/src/Vue/Main.java
@@ -1,16 +1,10 @@
 package Vue;
 
-import javax.swing.JFrame;
+public class Main {
 
-public class Main extends JFrame {
-	public Main(){
-		super("L3 INFOGame");
-		this.setSize(500,500);
-		this.setVisible(true);
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Main main=new Main();
+		System.out.println("coucou");
 	}
 
 }
